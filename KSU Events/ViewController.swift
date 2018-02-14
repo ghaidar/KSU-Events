c2e4c7215c82e61ce01a2907e53515a6058cf99f
Ghaida <ghaida@ghydas-MacBook-Pro.local>
2018-02-14 12:47:57 +0300

    Initial Commit

---
+++ KSU Events/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KSU Events
+//
+//  Created by Ghaida on 2/14/18.
+//  Copyright © 2018 Ghaida. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
