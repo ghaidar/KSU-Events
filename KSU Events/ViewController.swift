343546cee6b7627a76fca7e75747c8a8e209a1c9
Ghaida <ghaida@ghydas-MacBook-Pro.local>
2018-02-20 12:04:31 +0300

    buttun color

--- KSU Events/ViewController.swift
+++ KSU Events/ViewController.swift
@@ -12,9 +12,13 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // button color
+        myButton.setGradientBackground(colorOne:UIColor(red:225.0/225.0, green:225.0/225.0, blue: 225.0/225.0, alpha:1.0), colorTwo: UIColor (red:50.0/225.0, green: 160.0/225.0, blue: 216.0/225.0, alpha:1.0))
+        
+        
     }
-
+    @IBOutlet weak var myButton: UIButton!
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
